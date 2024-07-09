117010b31b23973f037fc5fa5d08446b39ea9ac0
Ruzan <141943187+asstaut@users.noreply.github.com>
2024-07-09 19:57:03 +0545

    Initial Commit

---
+++ FEDS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FEDS
+//
+//  Created by rujan on 09/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
